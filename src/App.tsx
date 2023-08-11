dd9da9e63cdbe75aaaa9b244c32706f2d444f424
Jason Adhinarta <jasonice6@gmail.com>
2023-08-12 00:49:03 +0200

    feat: added audio and keypressing logging

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Header from "components/Header";
 import Test from "components/Test";
@@ -11,6 +12,25 @@ import "stylesheets/themes.scss";
 import CommandPallet from "components/CommandPallet";
 
 export default function App() {
+    const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+    useEffect(() => {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices
+                .getUserMedia({ audio: true })
+                .then((stream) => {
+                    console.log("getUserMedia supported.");
+                    mediaRecorderRef.current = new MediaRecorder(stream);
+                })
+                .catch((err) => {
+                    console.error(
+                        `The following getUserMedia error occurred: ${err}`
+                    );
+                });
+        } else {
+            console.log("getUserMedia not supported on your browser!");
+        }
+    }, []); // Empty dependency array ensures this runs only on component mount
+
     const {
         time: { timerId, timer },
         word: { currWord, typedWord, activeWordRef },
@@ -60,6 +80,79 @@ export default function App() {
             dispatch(setTimerId(null));
         }
     }, [dispatch, timer, timerId]);
+
+    const [isRecording, setIsRecording] = useState(false);
+    const [startTimestamp, setStartTimestamp] = useState<number | null>(null);
+    const [recordedAudioChunks, setRecordedAudioChunks] = useState<Blob[]>([]);
+    const [recordedKeypresses, setRecordedKeypresses] = useState<any[]>([]);
+
+    useEffect(() => {
+        if (isRecording) {
+            const handleKeydown = (e: KeyboardEvent) => {
+                const relativeTimestamp = Date.now() - (startTimestamp || 0); // unnecessary || 0
+                recordedKeypresses.push({
+                    type: "keydown",
+                    key: e.key,
+                    timestamp: relativeTimestamp,
+                });
+            };
+
+            const handleKeyup = (e: KeyboardEvent) => {
+                const relativeTimestamp = Date.now() - (startTimestamp || 0); // unnecessary || 0
+                recordedKeypresses.push({
+                    type: "keyup",
+                    key: e.key,
+                    timestamp: relativeTimestamp,
+                });
+            };
+
+            document.addEventListener("keydown", handleKeydown);
+            document.addEventListener("keyup", handleKeyup);
+
+            return () => {
+                document.removeEventListener("keydown", handleKeydown);
+                document.removeEventListener("keyup", handleKeyup);
+            };
+        }
+    }, [isRecording, startTimestamp, recordedKeypresses]);
+
+    useEffect(() => {
+        const mediaRecorder = mediaRecorderRef.current;
+        if (timerId && !isRecording && mediaRecorder) {
+            mediaRecorder.ondataavailable = (event) => {
+                recordedAudioChunks.push(event.data);
+            };
+
+            mediaRecorder.onstop = () => {
+                const audioBlob = new Blob(recordedAudioChunks, {
+                    type: "audio/wav",
+                });
+                const audioUrl = URL.createObjectURL(audioBlob);
+                // You can use the audioUrl here to play or store the audio
+                console.log(audioUrl);
+                console.log(recordedKeypresses);
+                setRecordedAudioChunks([]);
+                setRecordedKeypresses([]);
+            };
+            console.log("start recording");
+
+            mediaRecorder.start();
+            setStartTimestamp(Date.now()); // Record the starting time
+            setIsRecording(true);
+        }
+
+        if (!timerId && isRecording && mediaRecorder) {
+            mediaRecorder.stop();
+            setStartTimestamp(null);
+            setIsRecording(false);
+        }
+    }, [
+        timerId,
+        isRecording,
+        mediaRecorderRef,
+        recordedAudioChunks,
+        recordedKeypresses,
+    ]);
 
     return (
         <>
